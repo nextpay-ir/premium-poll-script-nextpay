e072cba3e9be60c0ab21cb2fd3d07a5d09c9b355
Farzad Latifnia <register.site0@gmail.com>
2018-11-04 12:19:26 +0330

    Changed Default

--- themes/light/404.php
+++
@@ -1,9 +0,0 @@
-<?php defined("APP") or die() ?>
-<section>
-  <div class="container">
-  	<div class="centered is404">
-			<h1>404</h1>
-			<h2><?php echo e("Not Found.")?></h2>
-  	</div>
-  </div>
-</section>
